9cbdb8b2de204ffb5ce8110c2939ecc5827495fb
shrawanidandekar2 <130834977+shrawanidandekar2@users.noreply.github.com>
2023-06-04 23:43:36 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,22 @@
+package homew;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		driver.findElement(By.id("logoutLink")).click();	
+
+	}
+
+}
